87f131a33baa2544c2df1dc8da088314180487e9
samme <samme@users.noreply.github.com>
2018-10-10 09:54:08 -0700

    1.3.1

--- DebugArcadePhysics.js
+++ DebugArcadePhysics.js
@@ -1,6 +1,6 @@
 
 /*
-  Debug Arcade Physics plugin v1.3.0 for Phaser
+  Debug Arcade Physics plugin v1.3.1 for Phaser
  */
 
 (function() {
@@ -68,7 +68,7 @@
       return obj.type === SPRITE;
     };
 
-    DebugArcadePhysics.VERSION = "1.3.0";
+    DebugArcadePhysics.VERSION = "1.3.1";
 
     TOO_BIG = 9999;
 
